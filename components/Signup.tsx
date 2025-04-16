64dca2acd42aee062b246b4e38e6ab8db6b6ee82
alfiyafatima09 <alfiyafatima200431@gmail.com>
2025-04-17 02:12:02 +0530

    hatao ye lodash

--- components/Signup.tsx
+++ components/Signup.tsx
@@ -5,6 +5,7 @@ import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../Firebase'; 
 import { doc, setDoc } from 'firebase/firestore';
 import '../app/globals.css';
+// import { method } from 'lodash';
 
 
 const Signup = () => {
