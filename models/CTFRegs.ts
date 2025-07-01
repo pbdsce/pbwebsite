43d03f618dcf788e9709bcfb38ed58aed681f2c9
kamini08 <kaminibanait03@gmail.com>
2025-07-02 05:07:35 +0530

    switch to mongo for ctf registration

--- models/CTFRegs.ts
+++ models/CTFRegs.ts
@@ -59,6 +59,8 @@ const participantSchema = new Schema<Participant>({
   },
   background: { type: backgroundSchema, required: true },
   phone: { type: String, required: true },
+  branch: { type: String, required: true },
+  college: { type: String, required: true },
 });
 
 const registrationSchema = new Schema<Registration>({
