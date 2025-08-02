1ca43c4f92a285e79261897abed304f96f829889
ShreyashSri <shreyash.srivastava@pointblank.club>
2025-08-02 09:48:46 +0530

    minor fix

--- next.config.mjs
+++ next.config.mjs
@@ -32,15 +32,6 @@ const nextConfig = {
       },
     ];
   },
-  async redirects() {
-    return [
-      {
-        source: '/troll',
-        destination: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-        permanent: true,
-      },
-    ]
-  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
