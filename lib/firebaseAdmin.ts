c471c2db2828efa01fc8b65b7e63db21e8fd492f
saniyafatima07 <saniyafatima200631@gmail.com>
2025-10-02 11:47:55 +0530

    Fix build error on pr

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,15 +1,18 @@
 import * as admin from 'firebase-admin';
 
 const serviceAccountJson = process.env.FIREBASE_SERVICE_ACCOUNT_JSON;
-if (!serviceAccountJson) throw new Error("FIREBASE_SERVICE_ACCOUNT_JSON is not set");
 
-const serviceAccount = JSON.parse(serviceAccountJson);
+if (serviceAccountJson){
+  const serviceAccount = JSON.parse(serviceAccountJson);
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-}
+  if (!admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    });
+  } 
+} else {
+      console.log("Skipping Firebase initialization (no credentials available)");
+ }
 
 export const verifyFirebaseToken = async (token: string) => {
   try {
