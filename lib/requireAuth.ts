5e62abaadc656512dd77b30259dbed8e7c8bb467
saniyafatima07 <saniyafatima200631@gmail.com>
2025-10-04 01:35:17 +0530

    Add server side validation for domain check

--- lib/requireAuth.ts
+++ lib/requireAuth.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { verifyFirebaseToken } from "./firebaseAdmin";
 
+const ADMIN_DOMAIN = 'pointblank.club';
+
 export async function requireAuth(request: Request) {
   const authHeader = request.headers.get("Authorization");
   if (!authHeader?.startsWith("Bearer ")) {
@@ -10,6 +12,15 @@ export async function requireAuth(request: Request) {
   const token = authHeader.split(" ")[1];
   try {
     const decodedToken = await verifyFirebaseToken(token);
+    const userEmail = decodedToken.email;
+
+    if (!userEmail || !userEmail.endsWith(`@${ADMIN_DOMAIN}`)) {
+          return {
+              error: NextResponse.json({
+                  error: "Forbidden"
+              }, { status: 403 })
+        };
+    }  
     console.log("Decoded Token:", decodedToken);
     return { user: decodedToken };
   } catch (err) {
