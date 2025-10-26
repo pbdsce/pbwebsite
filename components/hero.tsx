0a99b4f01aaccc3264a5942973235b6aba4027a4
Shivansh Pandey <shivansh@Shivanshs-Mac-mini.local>
2025-10-26 16:18:34 +0530

    removed SIH banner

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,4 +1,8 @@
+import logo from "@/public/images/logo.svg";
+import Link from "next/link";
+import Image from "next/image";
 import FlickeringGrid from "@/components/magicui/flickering-grid";
+import { cn } from "@/lib/server/utils";
 import TypingAnimation from "@/components/ui/typing-animation";
 import "../app/css/additional-styles/landing.css";
 import RecruitmentBanner from "@/components/RecruitmentBanner";
