b758eef4e1353f63bf3363f886a4ecbd1080014d
Varchas H V <hvvarchas@gmail.com>
2026-01-27 21:24:25 +0530

    fix(UI): changes added to carousal component to add arrows and spacing added between carousal and text

--- components/ActivityCard.tsx
+++ components/ActivityCard.tsx
@@ -39,7 +39,7 @@ export default function ActivityCard({
 
 
        {/* TEXT — constrained */}
-       <div className="px-10 pu-10 lg:px-20">
+       <div className="px-10 lg:px-20">
          <div className="max-w-xl lg:ml-10">
            <h3 className="text-3xl md:text-4xl font-semibold tracking-tight text-green-400 pr-10">
              {title}
