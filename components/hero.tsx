ad16e879db1b1adb8fa9ed57a27dadb25b9f64d1
unknown <chauhandevansh780@gmail.com>
2026-02-01 18:47:15 +0530

    Final fixed version

--- components/hero.tsx
+++ components/hero.tsx
@@ -31,10 +31,10 @@ export default function Hero() {
                       data-aos-delay="450"
                     >
                       We are a student-run<br />
-                      <span className="bg-[#00c853] px-2  sm:px-3 sm:py-1 rounded-xl" style={{ wordBreak: 'keep-all' }}>tech-community</span> from Dayananda Sagar College of Engineering.
+                      <span className="bg-[#00c853] px-2  sm:px-3 sm:py-1 rounded-xl"style={{ wordBreak: 'keep-all' }}>tech-community</span> 
                     </p>
                   </div>
-                  <div className="flex flex-col items-center gap-3 p-1 hero-banners lg:w-[28rem]">
+                  <div className="flex flex-col items-center gap-3 p-1 transform scale-90 sm:scale-75 hero-banners">
                   {/* <RecruitmentBanner /> */}
                   {/*<SIHbanner />*/}
                   {/* <ZenithBanner /> */}
--- components/ui/NavItems.ts
+++ components/ui/NavItems.ts
@@ -3,6 +3,7 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 export const navItems = [
   { href: "https://github.com/pointblank-club", label: "GitHub", isExternal: true, icon: faGithub},
   // { href: "/recruitment", label: "Recruitment" },
+  { href : "/", label: "Home" },
   { href: "/events", label: "Events" },
   { href: "/leads", label: "Leads" },
   { href: "/lore", label: "Lore" },
@@ -12,3 +13,4 @@ export const navItems = [
 ];
 
 export default navItems;
+
