9761c0463b80f93aea8fe5797a5d2426c120680e
Devansh Chauhan <chauhandevansh780@gmail.com>
2026-02-15 18:45:53 +0530

    single source of distributions (#513)

---
+++ components/ui/NavItems.ts
@@ -0,0 +1,16 @@
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+export const navItems = [
+  { href: "https://github.com/pointblank-club", label: "GitHub", isExternal: true, icon: faGithub},
+  // { href: "/recruitment", label: "Recruitment" },
+  { href : "/", label: "Home" },
+  { href: "/events", label: "Events" },
+  { href: "/leads", label: "Leads" },
+  { href: "/lore", label: "Lore" },
+  { href: "/members", label: "Members", specialPadding: true },
+  { href: "/achievements", label: "Achievements" },
+  { href: "/hustle", label: "Hustle Results" },
+];
+
+export default navItems;
+
