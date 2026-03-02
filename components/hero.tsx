d6ce607e62c2eaf933d576f11c61756e8b7d99e3
Aditya Kumar <aditya4u164@gmail.com>
2026-03-02 15:59:49 +0530

    Remove Emoji

--- components/hero.tsx
+++ components/hero.tsx
@@ -31,7 +31,7 @@ export default function Hero() {
                       data-aos-delay="450"
                     >
                       We are a student-run<br />
-                      <span className="bg-[#00c853] px-2  sm:px-3 sm:py-1 rounded-xl" style={{ wordBreak: 'keep-all' }}>Open Source</span> Community from India.📍
+                      <span className="bg-[#00c853] px-2  sm:px-3 sm:py-1 rounded-xl" style={{ wordBreak: 'keep-all' }}>Open Source</span> Community from India.
                     </p>
                   </div>
                   <div className="flex flex-col items-center gap-3 p-1 hero-banners lg:w-[28rem]">
