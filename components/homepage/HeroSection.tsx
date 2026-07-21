9bf3e5d6025d0b4363a7455f6fd949ee8ed82684
Jayant Hegde Kageri <jayantkageri@gmail.com>
2026-07-22 00:10:34 +0530

    Remove PBCTF Banner

--- components/homepage/HeroSection.tsx
+++ components/homepage/HeroSection.tsx
@@ -3,7 +3,7 @@
 import { Lexend_Tera } from "next/font/google";
 import ThreeBackground from "@/components/ui/ThreeBackground";
 import { motion } from "framer-motion";
-import PbctfBanner from "@/components/ui/PbctfBanner";
+// import PbctfBanner from "@/components/ui/PbctfBanner";
 
 const lexendTera = Lexend_Tera({
   subsets: ["latin"],
@@ -11,11 +11,19 @@ const lexendTera = Lexend_Tera({
 });
 
 const headingParts = [
-  { text: "We Are ", className: "bg-linear-to-b px-1.5 from-[#FFFFFF] to-[#999999] bg-clip-text text-transparent  font-semibold" },
+  {
+    text: "We Are ",
+    className:
+      "bg-linear-to-b px-1.5 from-[#FFFFFF] to-[#999999] bg-clip-text text-transparent  font-semibold",
+  },
   { text: " <. >", className: "text-pbgreen font-semibold font-mono" },
   { br: true },
   { text: " Point ", className: "text-pbgreen font-semibold" },
-  { text: "Blank", className: "font-semibold px-1.5 bg-linear-to-b from-[#FFFFFF] to-[#999999] bg-clip-text text-transparent" },
+  {
+    text: "Blank",
+    className:
+      "font-semibold px-1.5 bg-linear-to-b from-[#FFFFFF] to-[#999999] bg-clip-text text-transparent",
+  },
   {
     text: "Student run Open Source Community from India",
     className: "text-base text-white italic pt-2",
@@ -30,7 +38,7 @@ export default function HeroSection() {
       className="relative z-10 min-h-[90vh] overflow-hidden text-white bg-pbpages"
     >
       <ThreeBackground />
-      <div className="relative flex justify-center lg:justify-between items-center min-h-[90vh] flex-col lg:flex-row gap-12 lg:gap-10 px-4 sm:px-10 lg:pl-24 lg:pr-10 xl:pr-16 py-32 lg:py-20 max-w-420 mx-auto w-full z-10">
+      <div className="relative flex justify-center items-center min-h-[90vh] flex-col gap-12 px-4 sm:px-10 lg:px-24 py-32 lg:py-20 max-w-420 mx-auto w-full z-10">
         <h1
           className={`text-4xl sm:text-5xl md:text-6xl xl:text-7xl text-center tracking-[-22%] text-white p-5 rounded-4xl select-none ${lexendTera.className}`}
         >
@@ -54,9 +62,6 @@ export default function HeroSection() {
             ),
           )}
         </h1>
-        <div className="flex w-full max-w-88 sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl flex-col gap-4 z-20">
-          <PbctfBanner />
-        </div>
       </div>
     </section>
   );
